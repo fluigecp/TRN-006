ec330886d3cff600e37f25988913c8f5260e734b
Lucas Pimenta <lucas.pimenta94@outlook.com>
2017-11-29 17:06:54 +0000

    ANALYTICS: Adicionando e tratando dados

--- forms/TRN-006/events/inputFields.js
+++ forms/TRN-006/events/inputFields.js
@@ -10,6 +10,28 @@ function inputFields(form){
             form.setValue("dataAprovacao", date);
         }
     }
+    // Informações do participante e treinamento.
+    form.setValue("custom_0", form.getValue("nomeParticipante") );
+    form.setValue("custom_1", form.getValue("matricula") );
+    form.setValue("custom_2", form.getValue("area") );
+    form.setValue("custom_3", form.getValue("cursoTreinamento") );
+    form.setValue("custom_4", form.getValue("classificacaoCurso") );
+    form.setValue("custom_5", form.getValue("instituicao") );
+    form.setValue("custom_6", form.getValue("cargaHoraria") );
+    form.setValue("custom_7", form.getValue("dataInicio") );
+    form.setValue("custom_8", form.getValue("dataTermino") );
+    // Informações avaliação.
+    form.setValue("custom_10", form.getValue("pergunta1") );
+    form.setValue("custom_11", form.getValue("pergunta2") );
+    form.setValue("custom_12", form.getValue("pergunta3_organizacao") );
+    form.setValue("custom_13", form.getValue("pergunta3_cargaHoraria") );
+    form.setValue("custom_14", form.getValue("pergunta3_material") );
+    form.setValue("custom_15", form.getValue("pergunta3_local") );
+    form.setValue("custom_16", form.getValue("pergunta4_comunicacao") );
+    form.setValue("custom_17", form.getValue("pergunta4_dominio") );
+    form.setValue("custom_18", form.getValue("pergunta4_relacionamento") );
+    form.setValue("custom_19", form.getValue("pergunta4_didatica") );
+    form.setValue("custom_20", form.getValue("pergunta4_habilidade") );
 }
 
 function getCurrentDate() {
