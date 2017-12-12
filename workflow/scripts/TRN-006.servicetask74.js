2a0fa50fd819cc60c6ce9f093d926d1295dc0107
Lucas Pimenta <lucas.pimenta94@outlook.com>
2017-12-12 15:11:49 +0000

    vinculando campos justificativa, anoTreinamento, id processo de origem ao serviço de integração de treinamentos

--- workflow/scripts/TRN-006.servicetask74.js
+++ workflow/scripts/TRN-006.servicetask74.js
@@ -1,135 +1,137 @@
 function servicetask74(attempt, message) {
- try {
-	var Service = ServiceManager.getService('ECMCardService');
-	log.warn('%%%%%% Service: ' + Service);
+	try {
+		var Service = ServiceManager.getService('ECMCardService');
+		log.warn('%%%%%% Service: ' + Service);
 
-	var serviceHelper = Service.getBean();
-	log.warn('%%%%%% serviceHelper: ' + serviceHelper);
+		var serviceHelper = Service.getBean();
+		log.warn('%%%%%% serviceHelper: ' + serviceHelper);
 
-	var serviceInstance = serviceHelper.instantiate("com.totvs.technology.ecm.dm.ws.ECMCardServiceService");
-	log.warn('%%%%%% serviceInstance: ' + serviceInstance);
+		var serviceInstance = serviceHelper.instantiate("com.totvs.technology.ecm.dm.ws.ECMCardServiceService");
+		log.warn('%%%%%% serviceInstance: ' + serviceInstance);
 
-	var cardFieldDtoArray = serviceHelper.instantiate("com.totvs.technology.ecm.dm.ws.CardFieldDtoArray");
-	log.warn('%%%%%% cardFieldDtoArray: ' + cardFieldDtoArray);
+		var cardFieldDtoArray = serviceHelper.instantiate("com.totvs.technology.ecm.dm.ws.CardFieldDtoArray");
+		log.warn('%%%%%% cardFieldDtoArray: ' + cardFieldDtoArray);
 
-	var portServico = serviceInstance.getCardServicePort();
-	log.warn('%%%%%% portServico: ' + portServico);
+		var portServico = serviceInstance.getCardServicePort();
+		log.warn('%%%%%% portServico: ' + portServico);
 
-	var user = hAPI.getAdvancedProperty("loginUserWS");
-	log.warn('%%%%%% user: ' + user);
+		var user = hAPI.getAdvancedProperty("loginUserWS");
+		log.warn('%%%%%% user: ' + user);
 
-	var password = hAPI.getAdvancedProperty("passwdUserWS");
-	log.warn('%%%%%% password: ' + password);
+		var password = hAPI.getAdvancedProperty("passwdUserWS");
+		log.warn('%%%%%% password: ' + password);
 
-	var empresa = parseInt(getValue("WKCompany"));
-	log.warn('%%%%%% empresa: ' + empresa + ' TypeOf: ' + typeof empresa);
+		var empresa = parseInt(getValue("WKCompany"));
+		log.warn('%%%%%% empresa: ' + empresa + ' TypeOf: ' + typeof empresa);
 
-	// Array com o nome de todos os campos a serem populados na ficha(participante e treinamento, respectivamente).
-	var fieldsFichaParticipante = ['matricula', 'participante', 'lotacao', 'departamento', 'campoDescritor'];
-	log.warn('%%%%%% fieldsFichaParticipante: ' + fieldsFichaParticipante);
+		// Array com o nome de todos os campos a serem populados na ficha(participante e treinamento, respectivamente).
+		var fieldsFichaParticipante = ['matricula', 'participante', 'lotacao', 'departamento', 'campoDescritor'];
+		log.warn('%%%%%% fieldsFichaParticipante: ' + fieldsFichaParticipante);
 
-	var cardFieldDto = serviceHelper.instantiate("com.totvs.technology.ecm.dm.ws.CardFieldDto");
-	log.warn('%%%%%% cardFieldDto: ' + cardFieldDto);
-
-	var matricula = hAPI.getCardValue("matricula");
-	log.warn('%%%%%% MATRICULA DO PARTICIPANTE: ' + matricula);
-
-	var numSolicitacaoTreinamento = hAPI.getCardValue("numSolicTreinamento");
-
-	// verifica se o participante possui ficha de treinamentos
-	var ficha = checkIfHasFicha(matricula);
-	log.warn('%%%%%% ficha: ' + ficha);
-
-	// Obtém os dados do participante no humanus
-	var humanusData = getParticipanteHumanusData(matricula);
-	log.warn('%%%%%% humanusData: ' + humanusData);
-
-	var fieldsFichaTreinamento = getArrayFieldNameTreinamento(ficha);
-	// objeto que irá conter os fields e os metadados referentes ao documento (create)
-	var cardDto = serviceHelper.instantiate("com.totvs.technology.ecm.dm.ws.CardDto"); // container com os dados de formulário e metadados
-	cardDto.setVersion(1000); //metadado que representa a versão do formulário      
-	cardDto.setParentDocumentId(28592); // metadado que representa a pasta referente ao formulário
-
-	// objeto que irá armazenar todos os campos e os respectivos valores (update)
-	var cardFieldDtoArray = serviceHelper.instantiate("com.totvs.technology.ecm.dm.ws.CardFieldDtoArray");
-
-	// get lotacao Info
-	var lotacaoInfo = getLotacaoInfo(parseInt(humanusData.getValue(0, "lotacao")));
-	// get departamento
-	var departamento = lotacaoInfo == "" ? lotacaoInfo : lotacaoInfo.getValue(0, "departamento");
-
-	// Array que armazena os valores de cada campo de participante
-	var arrayCardValuesParticipante = [
-		parseInt(humanusData.getValue(0, "matricula")),
-		humanusData.getValue(0, "nome"),
-		parseInt(humanusData.getValue(0, "lotacao")),
-		departamento,
-		humanusData.getValue(0, "nome")
-	];
-	log.warn('%%%%%% arrayCardValuesParticipante: ' + arrayCardValuesParticipante);
-
-	// Array que armazena os valores de cada campo de treinamento
-	var arrayCardValuesTreinamento = [
-		numSolicitacaoTreinamento,
-		hAPI.getCardValue("cursoTreinamento"),
-		hAPI.getCardValue("classificacaoCurso"),
-		"Não",
-		hAPI.getCardValue("instituicao"),
-		hAPI.getCardValue("justificativa"),
-		hAPI.getCardValue("cargaHoraria"),
-		hAPI.getCardValue("anoVigencia")
-	];
-
-	// execução do laço que irá popular o cardFieldDtoArray com os valores do participante
-	for (var y = 0; y < fieldsFichaParticipante.length; y++) {
-		// instanciando um cardFieldDto para representar um campo do formulário(chave e valor)
 		var cardFieldDto = serviceHelper.instantiate("com.totvs.technology.ecm.dm.ws.CardFieldDto");
-		//atribuindo a chave do campo
-		cardFieldDto.setField(fieldsFichaParticipante[y]);
-		//atribuindo valor ao campo
-		cardFieldDto.setValue(arrayCardValuesParticipante[y]);
-		// adicionando no objeto de valores - update
-		cardFieldDtoArray.getItem().add(cardFieldDto);
-		// adicionando no cardDto - create
-		cardDto.getCardData().add(cardFieldDto);
-	}
+		log.warn('%%%%%% cardFieldDto: ' + cardFieldDto);
 
-	for (var z = 0; z < fieldsFichaTreinamento.length; z++) {
-		// lógica para atribuir todos os valores de treinamento na tabela pai-filho de treinamentos do formulário.
-		// instanciando um cardFieldDto para representar um campo do formulário(chave e valor)
-		var cardFieldDto = serviceHelper.instantiate("com.totvs.technology.ecm.dm.ws.CardFieldDto");
-		//atribuindo a chave do campo
-		cardFieldDto.setField(fieldsFichaTreinamento[z]);
-		//atribuindo valor ao campo
-		cardFieldDto.setValue(arrayCardValuesTreinamento[z]);
-		// adicionando no objeto de valores - update
-		cardFieldDtoArray.getItem().add(cardFieldDto);
-		// adicionando no cardDto - create
-		cardDto.getCardData().add(cardFieldDto);
-	}
-	// se houver, atualiza a ficha. 
-	if (ficha != null) {
-		// objeto array que irá armazenar objetos CardFieldDto
-		log.warn('%%%%%% atualizando ficha ');
-		var docIdParticipante = ficha.getValue(0, "documentid");
-		// Verifica se o treinamento já foi cadastrado anteriormente. Caso não, ele adiciona o treinamento.
-		if ( !checkIfTreinamentoIsAlreadyRegistered(docIdParticipante, numSolicitacaoTreinamento) ) {
-			portServico.updateCardData(empresa, user, password, ficha.getValue(0, "documentid"), cardFieldDtoArray);
-			log.warn('%%%%%% ficha atualizada ');
+		var matricula = hAPI.getCardValue("matricula");
+		log.warn('%%%%%% MATRICULA DO PARTICIPANTE: ' + matricula);
+
+		var numSolicitacaoTreinamento = hAPI.getCardValue("numSolicTreinamento");
+
+		// verifica se o participante possui ficha de treinamentos
+		var ficha = checkIfHasFicha(matricula);
+		log.warn('%%%%%% ficha: ' + ficha);
+
+		// Obtém os dados do participante no humanus
+		var humanusData = getParticipanteHumanusData(matricula);
+		log.warn('%%%%%% humanusData: ' + humanusData);
+
+		var fieldsFichaTreinamento = getArrayFieldNameTreinamento(ficha);
+		// objeto que irá conter os fields e os metadados referentes ao documento (create)
+		var cardDto = serviceHelper.instantiate("com.totvs.technology.ecm.dm.ws.CardDto"); // container com os dados de formulário e metadados
+		cardDto.setVersion(1000); //metadado que representa a versão do formulário      
+		cardDto.setParentDocumentId(28592); // metadado que representa a pasta referente ao formulário
+
+		// objeto que irá armazenar todos os campos e os respectivos valores (update)
+		var cardFieldDtoArray = serviceHelper.instantiate("com.totvs.technology.ecm.dm.ws.CardFieldDtoArray");
+
+		// get lotacao Info
+		var lotacaoInfo = getLotacaoInfo(parseInt(humanusData.getValue(0, "lotacao")));
+		// get departamento
+		var departamento = lotacaoInfo == "" ? lotacaoInfo : lotacaoInfo.getValue(0, "departamento");
+
+		// Array que armazena os valores de cada campo de participante
+		var arrayCardValuesParticipante = [
+			parseInt(humanusData.getValue(0, "matricula")),
+			humanusData.getValue(0, "nome"),
+			parseInt(humanusData.getValue(0, "lotacao")),
+			departamento,
+			humanusData.getValue(0, "nome")
+		];
+		log.warn('%%%%%% arrayCardValuesParticipante: ' + arrayCardValuesParticipante);
+
+		var planejamentoAnual = hAPI.getCardValue("processIdOrigin") != "TRN-002" ? "Não" : "Sim";
+
+		// Array que armazena os valores de cada campo de treinamento
+		var arrayCardValuesTreinamento = [
+			numSolicitacaoTreinamento,
+			hAPI.getCardValue("cursoTreinamento"),
+			hAPI.getCardValue("classificacaoCurso"),
+			planejamentoAnual,
+			hAPI.getCardValue("instituicao"),
+			hAPI.getCardValue("justificativaTreinamento"),
+			hAPI.getCardValue("cargaHoraria"),
+			hAPI.getCardValue("anoTreinamento")
+		];
+
+		// execução do laço que irá popular o cardFieldDtoArray com os valores do participante
+		for (var y = 0; y < fieldsFichaParticipante.length; y++) {
+			// instanciando um cardFieldDto para representar um campo do formulário(chave e valor)
+			var cardFieldDto = serviceHelper.instantiate("com.totvs.technology.ecm.dm.ws.CardFieldDto");
+			//atribuindo a chave do campo
+			cardFieldDto.setField(fieldsFichaParticipante[y]);
+			//atribuindo valor ao campo
+			cardFieldDto.setValue(arrayCardValuesParticipante[y]);
+			// adicionando no objeto de valores - update
+			cardFieldDtoArray.getItem().add(cardFieldDto);
+			// adicionando no cardDto - create
+			cardDto.getCardData().add(cardFieldDto);
 		}
-	} else { // cria a ficha
-		log.warn('%%%%%% criando ficha ');
-		var cardDtoArray = serviceHelper.instantiate("com.totvs.technology.ecm.dm.ws.CardDtoArray");
-		cardDtoArray.getItem().add(cardDto);
-		portServico.create(empresa, user, password, cardDtoArray);
-		log.warn('%%%%%% ficha criada ');
+
+		for (var z = 0; z < fieldsFichaTreinamento.length; z++) {
+			// lógica para atribuir todos os valores de treinamento na tabela pai-filho de treinamentos do formulário.
+			// instanciando um cardFieldDto para representar um campo do formulário(chave e valor)
+			var cardFieldDto = serviceHelper.instantiate("com.totvs.technology.ecm.dm.ws.CardFieldDto");
+			//atribuindo a chave do campo
+			cardFieldDto.setField(fieldsFichaTreinamento[z]);
+			//atribuindo valor ao campo
+			cardFieldDto.setValue(arrayCardValuesTreinamento[z]);
+			// adicionando no objeto de valores - update
+			cardFieldDtoArray.getItem().add(cardFieldDto);
+			// adicionando no cardDto - create
+			cardDto.getCardData().add(cardFieldDto);
+		}
+		// se houver, atualiza a ficha. 
+		if (ficha != null) {
+			// objeto array que irá armazenar objetos CardFieldDto
+			log.warn('%%%%%% atualizando ficha ');
+			var docIdParticipante = ficha.getValue(0, "documentid");
+			// Verifica se o treinamento já foi cadastrado anteriormente. Caso não, ele adiciona o treinamento.
+			if (!checkIfTreinamentoIsAlreadyRegistered(docIdParticipante, numSolicitacaoTreinamento)) {
+				portServico.updateCardData(empresa, user, password, ficha.getValue(0, "documentid"), cardFieldDtoArray);
+				log.warn('%%%%%% ficha atualizada ');
+			}
+		} else { // cria a ficha
+			log.warn('%%%%%% criando ficha ');
+			var cardDtoArray = serviceHelper.instantiate("com.totvs.technology.ecm.dm.ws.CardDtoArray");
+			cardDtoArray.getItem().add(cardDto);
+			portServico.create(empresa, user, password, cardDtoArray);
+			log.warn('%%%%%% ficha criada ');
+		}
+
+
+	} catch (error) {
+		log.error(error);
+		throw error;
 	}
-
-
-} catch(error) { 
-	log.error(error);
-	throw error;
- }
 }
 
 /**
@@ -206,16 +208,15 @@ function getArrayFieldNameTreinamento(ficha) {
  * @returns - Boolean - true caso já exista, false caso ainda não exista.
  * 
  */
-function checkIfTreinamentoIsAlreadyRegistered(docId, numSolic){
+function checkIfTreinamentoIsAlreadyRegistered(docId, numSolic) {
 	var c1 = DatasetFactory.createConstraint("metadata#active", true, true, ConstraintType.MUST);
 	var c2 = DatasetFactory.createConstraint("documentid", docId, docId, ConstraintType.MUST);
 	var tablename = DatasetFactory.createConstraint("tablename", "treinamentos_realizados", "treinamentos_realizados", ConstraintType.MUST);
 	var fichaParticipante = DatasetFactory.getDataset("participante_x_treinamento", null, [c1, c2, tablename], null);
 	for (var i = 0; i < fichaParticipante.rowsCount; i++) {
-		if ( fichaParticipante.getValue(i, "numero_solicitacao_tb1") == numSolic ) {
+		if (fichaParticipante.getValue(i, "numero_solicitacao_tb1") == numSolic) {
 			return true;
 		}
 	}
 	return false;
 }
-
